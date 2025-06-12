f3223bf5b248c9942caef82fc0935a0f0d9a3e2f
João Arthur <joaoarthurce@gmail.com>
2025-06-12 15:54:44 -0300

    Adicionado

---
+++ scripts/parseConfig.js
@@ -0,0 +1,9 @@
+// Configuração do Parse/Back4App
+(function() {
+    Parse.initialize(
+        "O5RCNaudODvWDKuPigkfIpAx5mKtbxmkDrcXIFGh",
+        "rBo5DN9nzWQZAzVY679plKlUMGTyMGuzfN9gPVO2"
+    );
+    Parse.serverURL = 'https://parseapi.back4app.com/';
+    console.log('Parse configurado com sucesso');
+})();
